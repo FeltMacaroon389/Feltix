f17b5b389a6f354619bb16bbe126978f154e081f
Snorre Trehjørningen Svedman <snorre@svedman.no>
2025-04-28 19:20:39 +0200

    added ASCII art banner and modified welcome message

--- src/kernel.c
+++ src/kernel.c
@@ -5,9 +5,23 @@
 // Kernel main function
 void kernel_main() {
 
-    // Clear screen and print welcome message
+    // Clear screen
     clear_screen();
-    print_string("Welcome to Feltix!", VGA_COLOR_WHITE);
+
+    // Print ASCII art banner
+    print_string("  ___________    .__   __  .__                \n", VGA_COLOR_WHITE);
+    print_string("  \\_   _____/___ |  |_/  |_|__|__  ___       \n", VGA_COLOR_WHITE);
+    print_string("   |    __)/ __ \\|  |\\   __\\  \\  \\/  /   \n", VGA_COLOR_WHITE);
+    print_string("   |     \\  ___/|  |_|  | |  |>    <         \n", VGA_COLOR_WHITE);
+    print_string("   \\____/ \\____>____/__| |__/__/\\___\\     \n", VGA_COLOR_WHITE);
+
+    // Print welcome message
+    print_string("\n  Welcome to ", VGA_COLOR_WHITE);
+    print_string("Feltix!", VGA_COLOR_LIGHT_GREEN);
+
+    print_string("\n    By ", VGA_COLOR_WHITE);
+    print_string("FeltMacaroon389", VGA_COLOR_LIGHT_GREY);
+
 
     // Return (this shouldn't really happen)
     return;
