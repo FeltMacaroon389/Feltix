d40e962c30ce360116559fd1a75c5eca5548b90a
Snorre Trehjørningen Svedman <snorre@svedman.no>
2025-04-29 15:44:41 +0200

    seperate header for banners, for more modularity

---
+++ include/banner.h
@@ -0,0 +1,40 @@
+#ifndef BANNER_H
+#define BANNER_H
+
+#include <vga.h>
+
+// Banner lines definitions
+// Default banner generated with: https://www.asciiart.eu/text-to-ascii-art
+char banner_line_1[50] = "   _____    _ _   _              \n";
+char banner_line_2[50] = "  |  ___|__| | |_(_)_  __        \n";
+char banner_line_3[50] = "  | |_ / _ \\ | __| \\ \\/ /     \n";
+char banner_line_4[50] = "  |  _|  __/ | |_| |>  <         \n";
+char banner_line_5[50] = "  |_|  \\___|_|\\__|_/_/\\_\\    \n";
+
+// Print all-white banner
+void print_banner_white() {
+    print_string(banner_line_1, VGA_COLOR_WHITE);
+    print_string(banner_line_2, VGA_COLOR_WHITE);
+    print_string(banner_line_3, VGA_COLOR_WHITE);
+    print_string(banner_line_4, VGA_COLOR_WHITE);
+    print_string(banner_line_5, VGA_COLOR_WHITE);
+}
+
+// Print rainbow banner
+void print_banner_rainbow() {
+    print_string(banner_line_1, VGA_COLOR_RED);
+    print_string(banner_line_2, VGA_COLOR_LIGHT_RED);
+    print_string(banner_line_3, VGA_COLOR_GREEN);
+    print_string(banner_line_4, VGA_COLOR_BLUE);
+    print_string(banner_line_5, VGA_COLOR_MAGENTA);
+}
+
+
+/* Please feel free to request other options or color patterns 
+ * To do so, open an issue or pull request on the project's GitHub:
+ * https://github.com/FeltMacaroon389/Feltix
+ */
+
+
+#endif // BANNER_H
+
--- src/kernel.c
+++ src/kernel.c
@@ -1,6 +1,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <vga.h>
+#include <banner.h>
 
 // Kernel main function
 void kernel_main() {
@@ -8,13 +9,9 @@ void kernel_main() {
     // Clear the screen
     clear_screen();
 
-    // Print ASCII art banner
-    // Generated with: https://www.asciiart.eu/text-to-ascii-art
-    print_string("   _____    _ _   _              \n", VGA_COLOR_WHITE);
-    print_string("  |  ___|__| | |_(_)_  __        \n", VGA_COLOR_WHITE);
-    print_string("  | |_ / _ \\ | __| \\ \\/ /     \n", VGA_COLOR_WHITE);
-    print_string("  |  _|  __/ | |_| |>  <         \n", VGA_COLOR_WHITE);
-    print_string("  |_|  \\___|_|\\__|_/_/\\_\\     \n", VGA_COLOR_WHITE);
+    // Print white ASCII art banner
+    // See <include/banner.h>
+    print_banner_white();
 
     // Print credits
     print_string("\n    By ", VGA_COLOR_WHITE);
