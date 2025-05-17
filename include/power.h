b8a6e84b790bfc2921e777d231447f63a76a5e6c
FeltMacaroon389 <feltmacaroon@proton.ne>
2025-05-18 00:36:34 +0200

    added header to control power

---
+++ include/power.h
@@ -0,0 +1,20 @@
+// Simple power control
+
+#ifndef POWER_H
+#define POWER_H
+
+#include <io.h>
+
+// Trigger a hardware reboot via the PS/2 controller
+static inline void reboot_system(void) {
+    outb(0x64, 0xFE);
+
+    // If that fails, try unknown instruction
+    __asm__ __volatile__("ud2");
+
+    // If that *still* doesn't work, settle with a halt
+    __asm__ __volatile__("hlt");
+}
+
+#endif // POWER_H
+
