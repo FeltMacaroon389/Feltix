0987524bc07bc9ae27fd22ac3a0030ea5477d5cb
FeltMacaroon389 <feltmacaroon@proton.me>
2025-05-17 14:54:02 +0200

    added memcmp

--- include/string.h
+++ include/string.h
@@ -41,6 +41,19 @@ void* memset(void* ptr, int value, size_t num) {
     return ptr;
 }
 
+// Minimal implementation of memcmp
+int memcmp(const void* ptr1, const void* ptr2, size_t num) {
+    const unsigned char* p1 = (const unsigned char*)ptr1;
+    const unsigned char* p2 = (const unsigned char*)ptr2;
+
+    for (size_t i = 0; i < num; i++) {
+        if (p1[i] != p2[i]) {
+            return p1[i] - p2[i];
+        }
+    }
+    return 0;
+}
+
 // Minimal implementation of strcmp
 int strcmp(const char* str1, const char* str2) {
     while (*str1 && (*str1 == *str2)) {
